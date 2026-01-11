f323ca5a81ecbffe1595a3c8f3a99456176f49b1
felipematheus1337 <lipehbr@gmail.com>
2026-01-11 14:37:07 -0300

    Adjusted the consumer to the payload being with 'Message'

--- src/main/java/com/registry/infra/listener/UserCreatedListener.java
+++ src/main/java/com/registry/infra/listener/UserCreatedListener.java
@@ -4,12 +4,13 @@ import com.registry.domain.TypeUserStatus;
 import com.registry.domain.User;
 import com.registry.infra.UserRepository;
 import jakarta.transaction.Transactional;
+import org.springframework.messaging.Message;
 import org.springframework.stereotype.Component;
 
 import java.util.function.Consumer;
 
 @Component
-public class UserCreatedListener implements Consumer<User> {
+public class UserCreatedListener implements Consumer<Message<User>> {
 
     private final UserRepository repository;
 
@@ -17,9 +18,11 @@ public class UserCreatedListener implements Consumer<User> {
         this.repository = repository;
     }
 
+
     @Override
-    @Transactional
-    public void accept(User user) {
+    public void accept(Message<User> userMessage) {
+        User user = userMessage.getPayload();
+        if ( user == null ) throw new RuntimeException("Error ao serializar.");
         user.setStatus(TypeUserStatus.ADMINISTRATOR);
         repository.save(user);
     }
