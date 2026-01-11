acb8d5fc9380ef44088025f79970b716e1129dbb
felipematheus1337 <lipehbr@gmail.com>
2026-01-11 13:53:39 -0300

    Created endpoints in the User Controller.

--- src/main/java/com/registry/infra/controller/UserController.java
+++ src/main/java/com/registry/infra/controller/UserController.java
@@ -1,21 +1,36 @@
 package com.registry.infra.controller;
 
+import com.registry.application.UserService;
 import com.registry.domain.User;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequestMapping("/api/v1")
 @RestController
 public class UserController {
 
+    private final UserService service;
+
+    public UserController(UserService service) {
+        this.service = service;
+    }
+
 
     @PostMapping
-    ResponseEntity<?> createUser(@RequestBody User user) {
+    ResponseEntity<User> createUser(@Valid @RequestBody User user) {
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.create(user));
 
     }
 
     @GetMapping
-    ResponseEntity<?> list() {
+    ResponseEntity<List<User>> list() {
+
+        return ResponseEntity.ok(service.list());
 
     }
 
