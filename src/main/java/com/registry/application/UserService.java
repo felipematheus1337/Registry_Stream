a0b2c3fa0af8688c4624194ccc86f57408074adc
felipematheus1337 <lipehbr@gmail.com>
2026-01-11 13:51:38 -0300

    Created methods for User Service.

--- src/main/java/com/registry/application/UserService.java
+++ src/main/java/com/registry/application/UserService.java
@@ -1,7 +1,28 @@
 package com.registry.application;
 
+import com.registry.domain.User;
+import com.registry.infra.UserRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserService {
+
+    private final UserRepository repository;
+
+    public UserService(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    @Transactional
+    public User create(User u) {
+        return repository.save(u);
+    }
+
+    public List<User> list() {
+        return repository.findAll();
+    }
+
 }
